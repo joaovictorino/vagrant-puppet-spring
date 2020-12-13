381cf42f64bc40746c69f21079f8fe24b5fa2938
João Henrique Victorino <jh_vs@hotmail.com>
2020-12-13 15:33:29 -0300

    mysql

--- mysql/mysql_plugin.pp
+++ mysql/mysql_plugin.pp
@@ -1,18 +1,18 @@
 class { '::mysql::server':
-  restart                 => true,
-  override_options        => {
-    mysqld => {
-      'bind_address' => '0.0.0.0',
+  restart => true,
+  override_options => {
+    'mysqld' => {
+      'bind_address' => '0.0.0.0'
     }
   }
 }
 
 mysql::db { 'petclinic':
-  user     => 'petclinic',
+  user => 'petclinic',
   password => 'petclinic',
-  host     => '%',
-  grant    => ['ALL PRIVILEGES'],
+  host => '%',
+  grant => ['ALL PRIVILEGES'],
   require => Service['mysqld'],
-  sql     => [ '/vagrant/mysql/script/schema.sql',
-               '/vagrant/mysql/script/data.sql' ]
+  sql => [ '/vagrant/mysql/script/schema.sql',
+           '/vagrant/mysql/script/data.sql' ]
 }
